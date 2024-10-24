fceb51c4a1615c18018776addbb7535df1de2368
Emi1213 <emi1213galarza@gmail.com>
2024-10-24 15:11:54 -0500

    Add api routes to get the expense, incomes and total

--- shared/api-routes/api-routes.ts
+++ shared/api-routes/api-routes.ts
@@ -5,4 +5,22 @@ export const API_ROUTES = {
     LOGIN: "/auth/login",
     LOGOUT: "/auth/logout",
   },
+  EXPENSES: {
+    GET_EXPENSES: (userId: number, year: number, month: number) =>
+      `/expenses/${userId}/${year}/${month}`,
+    ADD_EXPENSE: "/expenses",
+    DELETE_EXPENSE: "/expenses",
+    UPDATE_EXPENSE: "/expenses",
+  },
+  INCOMES: {
+    GET_INCOMES: (userId: number, year: number, month: number) =>
+      `/incomes/${userId}/${year}/${month}`,
+    ADD_INCOME: "/incomes",
+    DELETE_INCOME: "/incomes",
+    UPDATE_INCOME: "/incomes",
+  },
+  TOTAL: {
+    GET_TOTAL: (userId: number, year: number, month: number) =>
+      `/total/${userId}/${year}/${month}`,
+  },
 };
